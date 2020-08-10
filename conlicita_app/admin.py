459d4b8902478dd01621ad6477d1d18039702699
Egor Rodrigues <egor.rodrigues@gmail.com>
2020-08-10 16:31:15 -0300

    criação do Model empresa

--- conlicita_app/admin.py
+++ conlicita_app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from conlicita_app.models import licitacao
+from conlicita_app.models import licitacao, empresa
 
 
 class licitacaoAdmin(admin.ModelAdmin):
     list_display = ('data_validade', 'public_body', 'modality', 'valor_estimado')
 
 
+
+
 admin.site.register(licitacao, licitacaoAdmin)
+admin.site.register(empresa)
--- conlicita_app/models.py
+++ conlicita_app/models.py
@@ -40,5 +40,30 @@ class licitacao(models.Model):
     modality = models.CharField(max_length=150, null=True, blank=True)
     # followups =
 
+    class Meta:
+        verbose_name_plural = 'licitações'
+
     def __str__(self):
         return f'{self.data_validade.strftime("%d/%m/%Y")} - {self.public_body} | {self.modality}'
+
+
+class empresa(models.Model):
+    razao_social = models.CharField(max_length=150)
+    cnpj = models.IntegerField()
+    endereco = models.CharField(max_length=150, null=True, blank=True)
+    cidade = models.CharField(max_length=50, null=True, blank=True)
+    estado = models.CharField(max_length=2, null=True, blank=True)
+    # telefone =
+    email = models.EmailField(max_length=254, null=True, blank=True)
+    situacao = models.CharField(max_length=20, null=True, blank=True)
+    tipo = models.CharField(max_length=20, null=True, blank=True)
+    natureza = models.CharField(max_length=50, null=True, blank=True)
+    abertura = models.DateField(null=True, blank=True)
+    capital_social = models.DecimalField(max_digits=11, decimal_places=2, null=True, blank=True)
+    # sócios =
+    segmento = models.TextField()
+
+
+    def __str__(self):
+        return f'{self.cnpj} - {self.razao_social}'
+
